84ba734d9447500352a1f49e4295e860d03b980f
Frank Edwards <frankedwards50@gmail.com>
2024-03-24 17:35:41 -0700

    Navbar Done

--- client/src/components/general-page-layout/navbar/Navbar.tsx
+++ client/src/components/general-page-layout/navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 // Library Imports
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 // Functions, Helpers, Utils, and Hooks
 import useWindowWidth from "../../../hooks/useWindowWidth";
@@ -13,39 +13,41 @@ import logo from "../../../assets/images/logo/logo.png";
 const Navbar: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
   const windowWidth = useWindowWidth();
-  
+
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
   return (
-    <nav className="navbar">
+    <nav className="navbar display-flex align-items-center">
       <div className="navbar-brand">
-        <Link to="/">Your Logo</Link>
+        <Link to="/">
+          <img src={logo} alt="Logo" />
+        </Link>
         {windowWidth <= 768 && (
           <button className="navbar-toggle" onClick={toggleMenu}>
             <span className="navbar-toggle-icon">&#9776;</span>
           </button>
         )}
       </div>
-      {(isOpen || windowWidth > 768) && (
-        <div className="navbar-menu">
-          <ul>
-            <li>
-              <AnimatedNavLink linkText="Home" url="/" />
-            </li>
-            <li>
-              <AnimatedNavLink linkText="About" url="/about" />
-            </li>
-            <li>
-              <AnimatedNavLink linkText="Contact Us" url="/contact-us" />
-            </li>
-            {/* Add more menu items */}
-          </ul>
-        </div>
-      )}
+      <div
+        className={`navbar-menu${isOpen ? " navbar-menu-open" : ""}`}
+      >
+        <ul className="space-around-flex align-items-center">
+          <li>
+            <AnimatedNavLink linkText="Home" url="/" />
+          </li>
+          <li>
+            <AnimatedNavLink linkText="About" url="/about" />
+          </li>
+          <li>
+            <AnimatedNavLink linkText="Contact Us" url="/contact-us" />
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
+
 
 export default Navbar;
