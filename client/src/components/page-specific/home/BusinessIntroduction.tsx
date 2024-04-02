7072fb16c1d0b5a93ff29ed55b2fa6598cc2736b
Frank Edwards <frankedwards50@gmail.com>
2024-04-02 13:39:40 -0700

    Home Page animations for tiers/introduction

--- client/src/components/page-specific/home/BusinessIntroduction.tsx
+++ client/src/components/page-specific/home/BusinessIntroduction.tsx
@@ -5,16 +5,20 @@ import { Button } from "../../button/Button";
 const BusinessIntroduction = () => {
   return (
     <section className="business-introduction padding-left-and-right padding-top-40 padding-bottom-80">
-      <PageHeader title="What We Provide" />
-      
-      <p>
-        Our websites are fully optimized for mobile devices, providing a
-        seamless browsing experience on any screen size. Plus, our future-proof
-        solutions ensure your website stays ahead of the curve in the
-        ever-changing digital landscape.
-      </p>
-      <br />
-      <p>Let's modernize and grow your business together. Contact us today!</p>
+      <div className="business-introduction-text-wrapper">
+        <PageHeader title="What We Provide" />
+
+        <p>
+          Our websites are fully optimized for mobile devices, providing a
+          seamless browsing experience on any screen size. Plus, our
+          future-proof solutions ensure your website stays ahead of the curve in
+          the ever-changing digital landscape.
+        </p>
+        <br />
+        <p>
+          Let's modernize and grow your business together. Contact us today!
+        </p>
+      </div>
 
       <div className="button-wrapper padding-top-40">
         <Button
