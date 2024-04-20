4b8803fc908e0be73cc433a12cb0a50d376a3c93
Frank Edwards <frankedwards50@gmail.com>
2024-04-20 11:21:05 -0700

    Available Tiers Prices added, verbiage revised to include logo

--- client/src/components/page-specific/home/AvailableTiers.tsx
+++ client/src/components/page-specific/home/AvailableTiers.tsx
@@ -26,7 +26,7 @@ const AvailableTiers = () => {
       <div className="option-card-wrapper display-flex justify-content-center">
         <Card
           cardVariant="imageAndBody"
-          headerText="Social Hub"
+          headerText="Social Hub - $500"
           bodyText="This tier is perfect for individuals or businesses looking to create a simple online presence. /nWith our Basic Profile package, you'll get a personalized landing page, where you can showcase all your social media links in one place."
           buttonCount={1}
           button1Text="Get Started"
@@ -36,7 +36,7 @@ const AvailableTiers = () => {
         />
         <Card
           cardVariant="imageAndBody"
-          headerText="Static Showcase"
+          headerText="Static Showcase - $2000"
           bodyText="Take your online presence to the next level with our Static Showcase package./n Ideal for small businesses or portfolios, this tier offers a static webpage with sleek design and no server functionality. Perfect for displaying your brand or portfolio in a professional manner."
           buttonCount={0}
           button1Text="Get Started"
@@ -46,8 +46,8 @@ const AvailableTiers = () => {
         />
         <Card
           cardVariant="imageAndBody"
-          headerText="Interact Pro"
-          bodyText="Elevate your online experience with our Dynamic Engagement package. /nDesigned for businesses that require server interaction, this tier offers a fully functional webpage with dynamic content and features."
+          headerText="Interact Pro - Flexible Pricing"
+          bodyText="Elevate your online experience with our Dynamic Engagement package. /nDesigned for businesses that require server interaction, this tier offers a fully functional webpage with dynamic content and features along with a free logo."
           buttonCount={0}
           button1Text="Get Started"
           button1Link="/contact-us"
