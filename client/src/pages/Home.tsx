6b9eb71df2eab99992bc397485da595586c4fe88
Frank Edwards <frankedwards50@gmail.com>
2024-04-21 00:39:49 -0700

    Remove Un-needed code from Home Page post re-factor (flicker bug fixed)

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,14 +1,10 @@
 // Library Imports
 import { Helmet, HelmetProvider } from "react-helmet-async";
-//import { useSelector } from "react-redux";
-import { useEffect, useState } from "react";
 // Functions, Helpers, Utils and Hooks
-import useDeviceInfo from "../hooks/useDeviceInfo";
 // Constants
 // Components
 import NavBar from "../components/general-page-layout/navbar/Navbar";
 import Footer from "../components/general-page-layout/footer/Footer";
-import { Loader } from "../components/general-page-layout/loader/Loader";
 import Hero from "../components/page-specific/home/Hero";
 import BusinessIntroduction from "../components/page-specific/home/BusinessIntroduction";
 import AvailableTiers from "../components/page-specific/home/AvailableTiers";
@@ -20,61 +16,6 @@ import "../css/page-specific/home.scss";
 */
 
 const Home = () => {
-  const userInfo = useDeviceInfo();
-  const isDev = import.meta.env.VITE_IS_DEV;
-
-  // Array of image URLs to preload
-  const [imageUrls, setImageUrls] = useState<string[]>([]);
-
-  useEffect(() => {
-    if (userInfo.device === "Desktop") {
-      setImageUrls([
-        "/assets/images/svgs/layered-waves/desktop/layered-waves.svg",
-        "/assets/images/svgs/layered-waves/desktop/layered-waves-2.svg",
-        "/assets/images/svgs/layered-waves/desktop/layered-waves-3.svg",
-        "/assets/images/svgs/layered-waves/desktop/layered-waves-4.svg",
-        "/assets/images/svgs/layered-waves/desktop/layered-waves-5.svg",
-        "/assets/images/svgs/layered-waves/desktop/layered-waves-6.svg",
-        "/assets/images/svgs/layered-waves/desktop/layered-waves-7.svg",
-      ]);
-    } else if (userInfo.device === "Mobile") {
-      setImageUrls([
-        "./assets/images/svgs/layered-waves/mobile/layered-waves.svg",
-        "./assets/images/svgs/layered-waves/mobile/layered-waves-2.svg",
-        "./assets/images/svgs/layered-waves/mobile/layered-waves-3.svg",
-        "./assets/images/svgs/layered-waves/mobile/layered-waves-4.svg",
-        "./assets/images/svgs/layered-waves/mobile/layered-waves-5.svg",
-        "./assets/images/svgs/layered-waves/mobile/layered-waves-6.svg",
-        "./assets/images/svgs/layered-waves/mobile/layered-waves-7.svg",
-      ]);
-    }
-  }, [userInfo, isDev]);
-
-  // State to track the loading status of images
-  const [imagesLoaded, setImagesLoaded] = useState(false);
-
-  // Function to preload images
-  useEffect(() => {
-    /* const loadImage = (url: string) => {
-      return new Promise((resolve, reject) => {
-        const img = new Image();
-        img.onload = () => resolve(undefined);
-        img.onerror = () => reject();
-        img.src = url;
-      });
-    }; */
-
-    const preloadImages = async () => {
-      try {
-        //await Promise.all(imageUrls.map(loadImage));
-        setImagesLoaded(true);
-      } catch (error) {
-        console.error("Error loading images:", error);
-      }
-    };
-    preloadImages();
-  }, [imageUrls]);
-
   return (
     <div className="home-page">
       <HelmetProvider>
@@ -119,17 +60,9 @@ const Home = () => {
       </HelmetProvider>
 
       <NavBar />
-      {!imagesLoaded ? (
-        <>
-          <Loader variant="primary" />
-        </>
-      ) : (
-        <>
-          <Hero />
-          <BusinessIntroduction />
-          <AvailableTiers />
-        </>
-      )}
+      <Hero />
+      <BusinessIntroduction />
+      <AvailableTiers />
 
       <Footer />
     </div>
